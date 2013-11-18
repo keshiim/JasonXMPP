f8c9f814deb506def1049f6ed86200b61f7f8c0e
suiwei <evenfate@gmail.com>
2013-11-18 11:08:13 +0800

    Initial Commit

---
+++ YGLXMPP/YGLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YGLAppDelegate.h
+//  YGLXMPP
+//
+//  Created by ygl10 on 13-11-18.
+//  Copyright (c) 2013年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YGLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YGLXMPP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YGLXMPP
+//
+//  Created by ygl10 on 13-11-18.
+//  Copyright (c) 2013年 ygl10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YGLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YGLAppDelegate class]));
+    }
+}
